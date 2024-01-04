8e360be86819cb8ff8e127fbc0c3d6a1a6af0a5e
Harsh-Vaishani <harshvaishnani8@gmail.com>
2024-01-04 11:56:30 +0530

    circlrt

---
+++ index.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int main()
+{
+
+
+                                                         // Question-1
+
+
+    for(int i=1; i<=7; i++){
+        printf(" * " );
+    }
+    printf("\n");
+    for(int i=1; i<=7; i++){
+        printf(" * " );
+    }
+    printf("\n");
+    for(int i=1; i<=7; i++){
+        printf(" * " );
+    }
+    printf("\n");
+    for(int i=1; i<=7; i++){
+        printf(" * " );
+    }
+    printf("\n");
+
+
+
+                                            // Question-2
+
+
+
+    // int num;
+    // printf("Number ");
+    // scanf("%d",&num);
+    // printf("Enter The Value ");
+    // scanf("%d",&i);
+    // for(int i=1; i<=30; i++){
+    //     if("i%2"){
+    //         printf("This Is Odd");
+    //     }
+    //     else{
+    //         printf("Even");
+    //     }
+    // }
+    
+ 
+
+   for(int i=1; i<=30;  i++){
+    printf("%d\n",i);
+}
+
+
+
+
+
+    return 0;
+}
